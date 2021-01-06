71457c8915bd8d983e79189245b59ec12affaf6e
Vishu1506 <77052702+Vishu1506@users.noreply.github.com>
2021-01-06 16:05:01 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ff58b87d97277001a0876e2",
+				"5ff58b87d97277001a0876e4"
+			]
+		},
+		"5ff58b87d97277001a0876e2": {
+			"id": "5ff58b87d97277001a0876e2",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ff58b87d97277001a0876e4": {
+			"id": "5ff58b87d97277001a0876e4",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
